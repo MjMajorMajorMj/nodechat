473bcb3e3953c704407773975fcd49bfad6fbe0e
MjMajorMajorMj <techk8@gmail.com>
2018-10-18 13:15:35 -0700

    Split script and css to separate files

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(initializeApp);
+
+function initializeApp() {
+    console.log("Chat started");
+    var socket = io();
+    $('form').submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+    });
+}
